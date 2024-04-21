b690f92237dffcf2774dbba1c7dd29cb1e03ea5c
huuhoang0601 <jinpaxencode@gmail.com>
2024-04-22 00:06:53 +0700

    add login

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Auth\RegisterController;
 
 // Khi người dùng truy cập '/', họ sẽ được chuyển hướng ngay lập tức đến trang đăng nhập.
 Route::get('/', function () {
-    return view('auth.login');
+    return view('login');
 });
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
